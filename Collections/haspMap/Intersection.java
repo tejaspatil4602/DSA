a97175fe1b6fea4d7b347523009b02bc5864deb5
tejaspatil4602 <tejaspatil4602@gmail.com>
2025-09-28 22:26:26 +0530

    Update

--- Collections/haspMap/Intersection.java
+++ Collections/haspMap/Intersection.java
@@ -1,5 +1,26 @@
 package Collections.haspMap;
 
-public class Intersection {
+import java.util.HashSet;
 
+public class Intersection {
+    public static int intersection(int[] arr1, int[] arr2 ){
+        int count = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i =0 ; i< arr1.length; i++){
+            set.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++){
+            if(set.contains(arr2[i])){
+                System.out.println(arr2[i]);
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {23,43,23,55,32,5};
+        int[] arr2 = { 23,34,55};
+        int res = intersection(arr1,arr2);
+        System.out.println("number of element are" + res);
+    }
 }
